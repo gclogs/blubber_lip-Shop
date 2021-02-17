eb88c51c8a354bf19a8df8dd47d72c722f14fe13
dev-chan06 <woig9874@naver.com>
2021-02-17 17:05:49 +0900

    something

--- js/EventListener.js
+++ js/EventListener.js
@@ -1,4 +1,17 @@
 
 function showProfile() {
-    let click = document.getElementById("profile");
+    let click = document.getElementById("profile").classList.toggle("show");
+}
+
+window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+        let dropdowns = document.getElementsByClassName("dropdown-content");
+        let i;
+        for (i = 0; i < dropdowns.length; i++) {
+            let openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
 }

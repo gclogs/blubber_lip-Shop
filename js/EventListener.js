678b16a48b13dfa6ee9a02612e086e963a730e6d
dev-chan06 <woig9874@naver.com>
2021-02-17 17:13:20 +0900

    initial commit.

--- js/EventListener.js
+++ js/EventListener.js
@@ -15,3 +15,10 @@ window.onclick = function(e) {
         }
     }
 }
+
+function showWarning() {
+    document.getElementById("profile").addEventListener("click", function() {
+        alert("아직 준비되지 않았습니다.");
+        return false;
+    })
+}
